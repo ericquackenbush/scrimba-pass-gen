0c4454c24c6400225a193817cb29f0d0c65b5f50
Eric Quackenbush <ericmquackenbush@gmail.com>
2022-04-30 14:49:34 -0400

    Created string containing character to generate the password from

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hello!")
+const lowercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const uppercase = "abcdefghijklmnopqrstuvwxyz"
+const digits = "0123456789"
+const alphabet = lowercase + uppercase + digits
